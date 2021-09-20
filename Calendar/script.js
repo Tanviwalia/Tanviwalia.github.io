7819af1417e39e76a8170c6a64944a31f79d0da7
Tanvi <waliatanvi2008@gmail.com>
2021-09-20 15:32:14 +0530

    update

---
+++ Calendar/script.js
@@ -0,0 +1,47 @@
+function myFunction1() {
+    alert("January");
+}
+
+function myFunction2() {
+    alert("February");
+}
+
+function myFunction3() {
+    alert("March");
+}
+
+function myFunction4() {
+    alert("April");
+}
+
+function myFunction5() {
+    alert("May");
+}
+
+function myFunction6() {
+    alert("June");
+}
+
+function myFunction7() {
+    alert("July");
+}
+
+function myFunction8() {
+    alert("August");
+}
+
+function myFunction9() {
+    alert("September");
+}
+
+function myFunction10() {
+    alert("October");
+}
+
+function myFunction11() {
+    alert("November");
+}
+
+function myFunction12() {
+    alert("December");
+}
